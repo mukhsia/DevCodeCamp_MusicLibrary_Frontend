5ea1f217378f1bcb35df6d7065234e00f50fd2a2
mukhsia <andre.n.mukhsia@gmail.com>
2023-12-07 17:26:24 -0800

    Initial Music Table skeleton.

--- src/components/MusicTable/MusicTable.jsx
+++ src/components/MusicTable/MusicTable.jsx
@@ -1,5 +1,27 @@
-const MusicTable = ({}) => {
-    return <div></div>;
+import SongItem from '../SongItem/SongItem';
+
+const MusicTable = ({ songs = [] }) => {
+    const songItems = songs.map((song) => (
+        <SongItem song={song} key={song.id} />
+    ));
+    return (
+        <div>
+            <h2>Music Table</h2>
+            <table className='table'>
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Artist</th>
+                        <th>Album</th>
+                        <th>Release Date</th>
+                        <th>Genre</th>
+                        <th>Likes</th>
+                    </tr>
+                </thead>
+                <tbody>{songItems}</tbody>
+            </table>
+        </div>
+    );
 };
 
 export default MusicTable;
