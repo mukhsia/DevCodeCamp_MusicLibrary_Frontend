4eb93aa6b46c2c379420e87cae05650a126499b0
mukhsia <andre.n.mukhsia@gmail.com>
2023-12-07 17:32:35 -0800

    Initial Song Item skeleton.

--- src/components/SongItem/SongItem.jsx
+++ src/components/SongItem/SongItem.jsx
@@ -1,5 +1,14 @@
-const SongItem = ({}) => {
-    return <div></div>;
+const SongItem = ({ song } = []) => {
+    return (
+        <tr>
+            <td>{song.title}</td>
+            <td>{song.artist}</td>
+            <td>{song.album}</td>
+            <td>{song.releaseDate}</td>
+            <td>{song.genre}</td>
+            <td>{song.likes}</td>
+        </tr>
+    );
 };
 
 export default SongItem;
