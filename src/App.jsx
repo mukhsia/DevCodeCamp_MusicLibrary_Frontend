87f756070ff7432eebe3a6395a9480e1159f9a33
mukhsia <andre.n.mukhsia@gmail.com>
2023-12-12 01:18:24 -0800

    Installed Axios and confirmed it is working to create MusicTable, displaying details of songs.

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,22 @@ import './App.css';
 import React, { useState, useEffect } from 'react';
 import Header from './components/Header/Header';
 import axios from 'axios';
-
-const fetchMusic = async () => {
-    try {
-        const response = await axios.get('https://localhost:7215/api/Songs');
-    } catch (error) {
-        console.warn('Error in fetching music.', error);
-    }
-};
+import MusicTable from './components/MusicTable/MusicTable';
 
 function App() {
+    const [songs, setSongs] = useState([]);
+
+    const fetchMusic = async () => {
+        try {
+            const response = await axios.get(
+                'https://localhost:7215/api/Songs'
+            );
+            setSongs(response.data);
+        } catch (error) {
+            console.warn('Error in fetching music.', error);
+        }
+    };
+
     useEffect(() => {
         fetchMusic();
     }, []);
@@ -19,6 +25,9 @@ function App() {
     return (
         <div className='App'>
             <Header />
+            <div>
+                <MusicTable songs={songs} />
+            </div>
         </div>
     );
 }
